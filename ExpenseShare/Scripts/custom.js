0e7e9c38f105c06033214f01cb519c20b81e95cf
Jimmy Vanetta <j_vanetta@yahoo.com>
2018-04-10 03:24:14 -0400

    implemented some validation on client side had the tip div only show on a button press.

--- ExpenseShare/Scripts/custom.js
+++ ExpenseShare/Scripts/custom.js
@@ -1,2 +1,35 @@
-﻿// Make query selectors for textboxes
-// Make entire text box highlight on lick
+﻿// Make query selectors for textbox
+$(document).ready(function () {
+
+    $("#tipSelect").hide();
+    let str = $("option:first");
+    if ($("#tipSelect").is(":visible")) {
+        str.html("No Tip");
+    }
+});
+
+
+
+
+
+function toggleTipSelect() {
+    let tipBtn = $(".tipBtn");
+    $("#tipSelect").slideToggle(1000);
+    if (tipBtn.text() == "Add A Tip") {
+        $(tipBtn).text("Hide");
+    }
+    else if (tipBtn.text() == "Hide") {
+        $(tipBtn).text("Add A Tip")
+    }
+}
+// Make entire text box clear on click
+$("#billCalcForm .textInput").focus(function () {
+    $(this).val("");
+});
+// Make entire text box ckear on click
+$("#currencyForm .textInput").focus(function () {
+    $(this).val("");
+});
+
+
+
