343933ce24363c6e3b4d24182ba6efb71948e3c0
Jimmy Vanetta <j_vanetta@yahoo.com>
2018-04-13 09:59:24 -0400

    changed total before tip to subtotal

--- ExpenseShare/Scripts/custom.js
+++ ExpenseShare/Scripts/custom.js
@@ -3,21 +3,27 @@ $(document).ready(function () {
     $("#tipSelect").hide();
 });
 
-
+// Toggle text inside of tip button
 function toggleTipSelect() {
-        let tipBtn = $(".tipBtn");
-        $("#tipSelect").slideToggle(500);
-        if (tipBtn.text() == "Add A Tip") {
-            $(tipBtn).text("Hide");
-        }
-        else if (tipBtn.text() == "Hide") {
-            $(tipBtn).text("Add A Tip")
-        }
+
+    let tipBtn = $(".tipBtn");
+
+    // toggle the tip menu
+    $("#tipSelect").slideToggle(500);
+    // if tip menu is hidden, button text is 'Add A Tip' 
+    if (tipBtn.text() == "Add A Tip") {
+        $(tipBtn).text("Hide");
     }
+    // if tip menu is visible, button text is 'Add A Tip'
+    else if (tipBtn.text() == "Hide") {
+        $(tipBtn).text("Add A Tip")
+    }
+};
+
 // Make entire text box clear on click
 $("#billCalcForm .textInput").focus(function () {
-        $(this).val("");
-    });
+    $(this).val("");
+});
 // Make entire text box ckear on click
 $("#currencyForm .textInput").focus(function () {
     $(this).val("");
